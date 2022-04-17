adce156a248c7d9f5f4e14337d7a8dc131c1ac40
Dhanraj <dhanrajthawani9@gmail.com>
2022-04-17 23:15:12 +0530

    second commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,53 +1,16 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+var createPostBtn = document.getElementById("createPostBtn");
 
-// Get the button that opens the modal
-var btn = document.getElementById("btn2");
+var closeCreatePost = document.getElementById("createPost-close");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var createPostModal = document.getElementById("createPost");
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+closeCreatePost.addEventListener("click", () => closeModal(createPostModal));
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+createPostBtn.addEventListener("click", () => openModal(createPostModal));
+
+window.addEventListener("click", function(event) {
+  if (event.target == createPostModal) {
+    createPostModal.style.display = "none";
   }
-}
-
-
-
-// Get the modal
-var modalo = document.getElementById("myModalo");
-
-// Get the button that opens the modal
-var btno = document.getElementById("btn3");
-
-// Get the <span> element that closes the modal
-var spano = document.getElementsByClassName("closeo")[0];
-
-// When the user clicks on the button, open the modal
-btno.onclick = function() {
-  modalo.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-spano.onclick = function() {
-  modalo.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modalo) {
-    modalo.style.display = "none";
-  }
-}
+});
